3db8977809761c5b737f467d607eeab2a1808a0f
Andreas040206 <andreasskakkebaekkruse@gmail.com>
2023-04-05 14:25:41 +0200

    I've created the logic that displays a loading function and saves the current location to local storage, i've also made all the el creating functions, promises for better performance

--- src/UI.js
+++ src/UI.js
@@ -1,41 +1,77 @@
+import { reject } from "lodash";
 import { btnActivation } from "./controller";
 import loadingGif from "./Img/Loading.gif";
 
-const createPage = () => {
+const createPage = async (data) => {
   const content = document.createElement("div");
   content.classList.add("content");
+  content.setAttribute("id", "content");
 
-  content.appendChild(createSearchBar());
+  // Here make a promise to return content ones its done
+  return new Promise((resolve, reject) => {
+    // We use promise.all([]) so every promise can finish at the same time
+    Promise.all([
+      createSearchBar(data),
+      createWeatherLocInfo(data),
+      loadBg(data),
+    ])
+      // When it's done we will append the two and resolve
+      .then((el) => {
+        content.appendChild(el[0]);
+        content.appendChild(el[1]);
 
-  document.body.appendChild(content);
-};
-
-const createSearchBar = () => {
-  const searchBarCon = document.createElement("form");
-  searchBarCon.classList.add("searchBarCon");
-
-  searchBarCon.setAttribute("onsubmit", "return false");
-
-  const searchBarInput = document.createElement("input");
-  searchBarInput.setAttribute("type", "text");
-  searchBarInput.classList.add("searchBarInput");
-
-  const searchBarBtn = document.createElement("button");
-  searchBarBtn.classList.add("searchBarBtn");
-  searchBarBtn.textContent = "Search";
-
-  searchBarBtn.addEventListener("click", () => {
-    btnActivation.searchBtn(searchBarInput.value);
-    searchBarInput.value = "";
+        resolve(content);
+      });
   });
-
-  searchBarCon.appendChild(searchBarInput);
-  searchBarCon.appendChild(searchBarBtn);
-
-  return searchBarCon;
 };
 
-const appendLoader = () => {
+const createSearchBar = (data) => {
+  return new Promise((resolve, reject) => {
+    const searchBarCon = document.createElement("form");
+    searchBarCon.classList.add("searchBarCon");
+
+    searchBarCon.setAttribute("onsubmit", "return false");
+
+    const searchBarInput = document.createElement("input");
+    searchBarInput.setAttribute("type", "text");
+    searchBarInput.classList.add("searchBarInput");
+    searchBarInput.value = data.location.name;
+
+    const searchBarBtn = document.createElement("button");
+    searchBarBtn.classList.add("searchBarBtn");
+    searchBarBtn.textContent = "Search";
+
+    searchBarBtn.addEventListener("click", () => {
+      if (searchBarInput.value !== "") {
+        btnActivation.searchBtn(searchBarInput.value);
+      }
+      searchBarInput.value = "";
+    });
+
+    searchBarCon.appendChild(searchBarInput);
+    searchBarCon.appendChild(searchBarBtn);
+
+    resolve(searchBarCon);
+  });
+};
+
+const createWeatherLocInfo = (data) => {
+  return new Promise((resolve, reject) => {
+    const InfoCon = document.createElement("div");
+
+    InfoCon.textContent = `${data.location.country}, ${data.location.name}`;
+
+    resolve(InfoCon);
+  });
+};
+
+const loadBg = (data) => {
+  return new Promise((resolve, reject) => {
+    resolve();
+  });
+};
+
+const appendLoadingEl = () => {
   const loadingGifCon = document.createElement("div");
   loadingGifCon.classList.add("loadingGifCon");
 
@@ -48,4 +84,4 @@ const appendLoader = () => {
   return loadingGifCon;
 };
 
-export { createPage, appendLoader };
+export { createPage, appendLoadingEl };
--- src/controller.js
+++ src/controller.js
@@ -1,7 +1,9 @@
-import { createPage, appendLoader } from "./UI";
+import { create, reject } from "lodash";
+import { createPage, appendLoadingEl } from "./UI";
 
 const getInfoFromAPI = async (location) => {
   try {
+    // Get data from API
     const response = await fetch(
       `https://api.weatherapi.com/v1/current.json?key=86c4e4787a1b49ee91a190852230304&q=${location}`,
       {
@@ -9,27 +11,97 @@ const getInfoFromAPI = async (location) => {
       }
     );
 
-    return response.json().then((data) => {
-      return data;
-    });
+    if (response.ok) {
+      // If data is OK, convert with .json() and return data
+      return response.json().then((data) => {
+        console.log(data);
+        return data;
+      });
+    } else {
+      // If data is NOT OK, then return null
+      return null;
+    }
   } catch {
+    // If any error occurs, throw one.
     (err) => {
       throw console.error(err);
     };
   }
 };
 
-const btnActivation = {
-  searchBtn: (val) => {
-    const loadingEl = appendLoader();
-    document.body.appendChild(loadingEl);
-    getInfoFromAPI(val).then((data) => {
-      console.log(data);
-      document.body.removeChild(loadingEl);
-    });
-  },
+const delay = (time) => {
+  // Lets return a promise, that we at some point
+  // will resolve or reject something
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, time);
+  });
 };
 
-createPage();
+const reloadPage = async (currentLocation) => {
+  // Appends The loading screen
+  const loadingEl = appendLoadingEl();
+  document.body.appendChild(loadingEl);
+
+  let location;
+
+  // If no Location is added, check local storage, else set "London"
+  if (currentLocation !== undefined) {
+    location = currentLocation;
+  } else if (!localStorage.getItem("Location")) {
+    localStorage.setItem("Location", "London");
+    location = "London";
+  } else {
+    location = localStorage.getItem("Location");
+  }
+
+  // GET data from API
+  const data = await getInfoFromAPI(location);
+
+  // Is there any data?
+  if (data !== null) {
+    // If there is data, lets add this location to local storage
+    localStorage.setItem("Location", currentLocation);
+
+    // If content already exist, remove it.
+    const content = document.querySelector("#content");
+    if (content) {
+      document.body.removeChild(content);
+    }
+
+    // Wait for the async funtion to create the page
+    const newPage = await createPage(data);
+
+    document.body.appendChild(newPage);
+  } else {
+    // If the data was not okey
+
+    const content = document.querySelector("#content");
+    // If no content exist on the page, lets add some
+    if (!content) {
+      // London is the standard here
+      const newData = await getInfoFromAPI("London");
+
+      const newPage = await createPage(newData);
+
+      document.body.appendChild(newPage);
+    }
+  }
+  loadingEl.style.opacity = "0";
+
+  // Only remove when opacity is 0
+  delay(1000).then(() => {
+    document.body.removeChild(loadingEl);
+  });
+};
+
+reloadPage();
+
+const btnActivation = {
+  searchBtn: (val) => {
+    reloadPage(val);
+  },
+};
 
 export { btnActivation };
